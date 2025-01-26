3b83c90e3367f45f59ca4638d8efa1a90bc2dfec
raja <rraja48569@gmail.com>
2025-01-26 11:45:47 +0530

    first commit

---
+++ Pandas_notes/basics.py
@@ -0,0 +1,13 @@
+#                  INTRO
+'''
+ * Pandas suits for tabular data with heterogeneously typed columns
+   are in SQL table or Excel sheet
+
+ * Ordered and unordered time series data.
+
+ * Series - 1D, DataFrame - 2D.
+
+ * Helps in reshaping data 
+'''
+import pandas as pd
+print(pd.__version__)
