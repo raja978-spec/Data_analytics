561a71c352aaaa4afe3b9706bb06760377d4bd6b
raja <rraja48569@gmail.com>
2025-01-28 20:52:05 +0530

    Numpy basics start

---
+++ Numpy_notes/numpy_basics.py
@@ -0,0 +1,12 @@
+#                USE OF NUMPY ARRAY
+'''
+ Used to do mathametical operation for ML, DS
+
+'''
+
+#             NP ARRAY
+import numpy as np
+
+a=[[1,3,1],[4,6,1]] # normal array will not have any operations
+nump = np.array(a)
+print(np.unique(a))
